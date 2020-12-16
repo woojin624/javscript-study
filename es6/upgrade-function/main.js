893861bf92f1c8dab69c763142fd97cc7e316132
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-12-16 17:10:11 +0900

    arguments

--- es6/upgrade-function/main.js
+++ es6/upgrade-function/main.js
@@ -27,3 +27,23 @@ function add3(a = 3, b = func1()) {
 
 add3(); // ---> add3(3, 3) ---> 6
 add3(5); // ---> add3(5, 3) ---> 8
+
+// ----------------------------------------------
+
+// 함수의 arguments
+
+function exam1(a, b, c) {
+  // ---> 함수의 인자값으로 들어간 것이 parameter
+  console.log(a, b, c); // ---> 콘솔 내부의 값으로 들어간 것이 argument
+}
+
+function func2(a, b, c) {
+  // arguments는 함수의 parameter에 들어간 것을 Array안에 담은 변수
+  console.log(arguments);
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+  console.log(arguments[2]);
+  // for문을 써서 arguments.length를 통해 파라미터의 갯수에 상관 없이 출력 가능
+}
+
+func2(1, 2, 3);
